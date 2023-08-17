3e7703578d7cdc418668ac970545b2c121a99485
brightt <faladetomiwa343@gmail.com>
2023-08-17 11:42:18 -0600

    first commit

---
+++ Tdd/Task2.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Task2
+{
+
+
+
+    @Test
+
+    public  void testTwiceCharacter(){
+
+    }
+
+
+
+}
